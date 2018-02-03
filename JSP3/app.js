eee5b6c38210b87eaaf9d1351ef89cce46123369
4ppleCracker <4pplecracker@gmail.com>
2018-02-03 13:39:49 +0100

    @fdef function definition example: @fdef MULTIPLY(a, b) a * b makes MULTIPLY(10, 10) result in 10 * 10

--- JSP3/app.js
+++ JSP3/app.js
@@ -12,6 +12,9 @@ RegExp.prototype.execAll = function (string) {
     }
     return matches;
 }
+function debug() {
+    return false
+}
 
 function defmatch(code) {
     var regex = /@def[ ]*([A-z0-9]*)[ ]*([^\r\n ]*)/g
@@ -22,7 +25,7 @@ function defmatch(code) {
         return b[1].length - a[1].length;
     })
     matcharray.forEach(function (match) {
-        console.log("replacing " + match[1] + " with " + match[2]);
+        if (debug()) console.log("replacing " + match[1] + " with " + match[2]);
 
         code = code.replace(regex, "");
         var reg = new RegExp("(" + match[1] + ")", "g");
@@ -33,13 +36,43 @@ function defmatch(code) {
     });
     return code;
 }
-var code = "@def TEST 123\n\
-@def TEST2 200\n\
-console.log(TEST)\n\
-console.log(TEST2 + 100)\n\
-console.log(TEST + TEST2) //TeST\n\
-TEST/a"
-console.log("Input code\n\n" + code)
-code = defmatch(code);
+function fdefmatch(code) {
+    var regex = /@fdef[ ]*([^\r\n ]*)\(([A-z, ]*)\)[ ]*([^\r\n]*)/g
 
-console.log("Output code" + code);
+    var matcharray = regex.execAll(code);
+
+    matcharray.sort(function (a, b) {
+        return b[1].length - a[1].length;
+    })
+    matcharray.forEach(function (match) {
+        if (debug()) console.log("replacing " + match[1] + " function");
+
+        code = code.replace(regex, "");
+        var regstring = match[1] + "\(([^\r\n\)]*)\)"//"\(([^\r\n\)]*)\)";
+        var reg = new RegExp(regstring, "g");
+        var t = reg.execAll(code)
+        t.forEach(function (ma) {
+            var param = ma[1].replace(/[\(\) ]*/g, "").split(",")
+            var funcparam = match[2].replace(/[ ]*/g, "").split(",");
+            var funccode = match[3];
+            funcparam.forEach(function (p, index) {
+                funccode = funccode.replace(p, param[index]);
+            });
+            var toreplace = ma[0]
+            code = code.replace(toreplace, "(" + funccode);
+        });
+    });
+    return code;
+}
+var code = "@fdef TIMESTWO(a) a * 2\n\
+@fdef ADD(a, b) a + b\n\
+@def NUM 10\n\
+console.log(TIMESTWO(NUM))\n\
+console.log(ADD(NUM, NUM))\n\
+console.log(TIMESTWO(NUM) + ADD(NUM,NUM))\n\
+TIMESTWO(NUM)";
+
+code = defmatch(code);
+code = fdefmatch(code);
+
+console.log("Output code:\n" + code.replace(/^\s*[\r\n]/gm, ""));
