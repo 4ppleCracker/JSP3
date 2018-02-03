9306596b5d76f8b370ac9d34416c11a8f0189cac
4ppleCracker <4pplecracker@gmail.com>
2018-02-03 14:52:34 +0100

    @ifdef @else @ifdef will execute code block if variable is defined @else will execute code block if @ifdef fails example @def TEST 10 @ifdef TEST {

--- JSP3/app.js
+++ JSP3/app.js
@@ -15,11 +15,23 @@ RegExp.prototype.execAll = function (string) {
 function debug() {
     return false
 }
-
+global.defineds = [];
 function defmatch(code) {
-    var regex = /@def[ ]*([A-z0-9]*)[ ]*([^\r\n ]*)/g
+    var defregex = /@def[ ]*([A-z0-9]*)[ ]*([^\r\n ]*)/g
 
-    var matcharray = regex.execAll(code);
+    var matcharray = defregex.execAll(code);
+
+    matcharray.sort(function (a, b) {
+        return b[1].length - a[1].length;
+    })
+    matcharray.forEach(function (match) {
+        global.defineds.push(match[1]);
+    });
+}
+function defreplace() {
+    var defregex = /@def[ ]*([A-z0-9]*)[ ]*([^\r\n ]*)/g
+
+    var matcharray = defregex.execAll(code);
 
     matcharray.sort(function (a, b) {
         return b[1].length - a[1].length;
@@ -27,7 +39,7 @@ function defmatch(code) {
     matcharray.forEach(function (match) {
         if (debug()) console.log("replacing " + match[1] + " with " + match[2]);
 
-        code = code.replace(regex, "");
+        code = code.replace(defregex, "");
         var reg = new RegExp("(" + match[1] + ")", "g");
         var t = reg.execAll(code);
         t.forEach(function (ma) {
@@ -64,15 +76,61 @@ function fdefmatch(code) {
     });
     return code;
 }
+function ifdefmatch(code) {
+    var regex = /@ifdef[ ]*([^{ ]*)[ ]*{[\n\r]*([^}]*)}/g
+
+    var matcharray = regex.execAll(code);
+
+    matcharray.sort(function (a, b) {
+        return b[1].length - a[1].length;
+    })
+    matcharray.forEach(function (match) {
+        if (debug()) console.log("");
+        console.log(match);
+        if (global.defineds.includes(match[1])) {
+            if (debug()) console.log("defined! replacing " + match[0] + " with " + match[2]);
+            code = code.replace(match[0], match[2]);
+        } else {
+            if (debug()) console.log("not defined! replacing " + match[0] + " with nothing");
+            code = code.replace(match[0], "");
+        }
+    });
+    return code;
+}
+function ifelsedefmatch(code) {
+    var regex = /@ifdef[ ]*([^{ ]*)[ ]*{[\n\r]*([^}]*)}[ ]*@else[ ]*{[\n\r]*([^}]*)}/g
+
+    var matcharray = regex.execAll(code);
+
+    matcharray.sort(function (a, b) {
+        return b[1].length - a[1].length;
+    })
+    matcharray.forEach(function (match) {
+        if (debug()) console.log("");
+        if (global.defineds.includes(match[1])) {
+            if (debug()) console.log("defined! replacing " + match[0] + " with " + match[2]);
+            code = code.replace(match[0], match[2]);
+        } else {
+            if (debug()) console.log("not defined! replacing " + match[0] + " with " + match[3]);
+            code = code.replace(match[0], match[3]);
+        }
+    });
+    return code;
+}
 var code = "@fdef TIMESTWO(a) a * 2\n\
 @fdef ADD(a, b) a + b\n\
 @def NUM 10\n\
-console.log(TIMESTWO(NUM))\n\
-console.log(ADD(NUM, NUM))\n\
-console.log(TIMESTWO(NUM) + ADD(NUM,NUM))\n\
-TIMESTWO(NUM)";
+@def SECNUM 20\n\
+@ifdef NOTNUM{\n\
+console.log(NUM + ADD(10,10))\n\
+} @else {\n\
+console.log(SECNUM + TIMESTWO(2))\n\
+}";
 
-code = defmatch(code);
+defmatch(code);
 code = fdefmatch(code);
+code = ifelsedefmatch(code);
+code = ifdefmatch(code);
+code = defreplace(code);
 
 console.log("Output code:\n" + code.replace(/^\s*[\r\n]/gm, ""));
